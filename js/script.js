dfa844d653c5684ec75345a7dd20679b4a74c8a9
Miike Ramos <mikeramos@gmail.com>
2011-10-24 01:10:58 -0700

    New search concept, focusing on regional selection first. Todo: Adjust display of areas

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@
 
 */
 
-
-
-
-
-
-
+function setCity(region) {
+	var regionName = region.value;
+	document.getElementById("area"+regionName).style.display = "block";
+	/*switch (regionName) {
+		case "Arizona":
+			//document.getElementById("areas").innerHTML = "<div>Optionally, would you like to search within a city?</div>";
+			document.getElementById("areaArizona").style.display = "block";
+			break;
+		default:
+			alert("No state selected");
+	}*/
+}
