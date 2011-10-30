3c9660c3693c04e53e526c57e3db4953e4d295ff
Miike Ramos <mikeramos@gmail.com>
2011-10-30 02:32:18 -0700

    This implementation of regions won't work

---
+++ js/search.js
@@ -0,0 +1,25 @@
+var regions = [
+	"Denver", [
+		"Centennial",
+		"Greenwood Village",
+		"Highlands Ranch",
+		"Littleton",
+		"Thornton",
+		"Westminster"],
+	"Phoenix", [
+		"Chandler",
+		"Phoenix"],
+	"Los Angeles County", [
+		"Los Angeles",
+		"Marina del Rey",
+		"Northridge",
+		"Pasadena",
+		"Santa Clarita",
+		"Valencia",
+		"Wilmington"],
+	"Inland Empire",
+	"Orange County",
+	"San Diego",
+	"Seattle",
+	"San Francisco",
+	"Sacramento"];
