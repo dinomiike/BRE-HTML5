3a26b4968eef34858d607a208d24f603d7a4b5df
Miike Ramos <mikeramos@gmail.com>
2011-10-30 21:14:11 -0700

    Frustration

--- js/search.js
+++ js/search.js
@@ -1,30 +1,100 @@
-var regions = [
-	"Denver", [
-		"Centennial",
-		"Greenwood Village",
-		"Highlands Ranch",
-		"Littleton",
-		"Thornton",
-		"Westminster"],
-	"Phoenix", [
-		"Chandler",
-		"Phoenix"],
-	"Los Angeles County", [
-		"Los Angeles",
-		"Marina del Rey",
-		"Northridge",
-		"Pasadena",
-		"Santa Clarita",
-		"Valencia",
-		"Wilmington"],
-	"Inland Empire",
-	"Orange County",
-	"San Diego",
-	"Seattle",
-	"San Francisco",
-	"Sacramento"];
+var regions = {
+	"Arizona": {
+		"Phoenix Area": [
+			{
+				"city": "Chandler"
+			},
+			{
+				"city": "Phoenix"
+			}
+		]
+	},
+	"California": {
+		"Inland Empire": {
+			"Chino": {},
+			"Chino Hills": {},
+			"Colton": {}
+		}
+	},
+	"Denver": {
 
-var regions2 = [
+	},
+	"Washington": {
+
+	}
+};
+
+var regions2 = {
+	"locations": [ 
+		{
+			"Arizona": [
+				{
+					"Phoenix Area": [
+						{
+							"city": "Chandler"
+						},
+						{
+							"city": "Phoenix"
+						}
+					]
+				}
+			]
+		},
+		{
+			"California": [
+				{
+					"Inland Empire": [
+						{
+							"city": "Chino"
+						},
+						{
+							"city": "Chino Hills"
+						},
+						{
+							"city": "Colton"
+						},
+						{
+							"city": "Diamond Bar"
+						},
+						{
+							"city": "Moreno Valley"
+						},
+						{
+							"city": "Riverside"
+						}
+					]
+				},
+				{
+					"Los Angeles County": [
+						{
+							"city": "Los Angeles"
+						},
+						{
+							"city": "Marina del Rey"
+						},
+						{
+							"city": "Northridge"
+						},
+						{
+							"city": "Pasadena"
+						},
+						{
+							"city": "Santa Clarita"
+						},
+						{
+							"city": "Valencia"
+						},
+						{
+							"city": "Wilmington"
+						}
+					]
+				}
+			]
+		}
+	]
+};
+
+/*var regions2 = [
 	["Denver", [
 		"Centennial",
 		"Greenwood Village",
@@ -35,4 +105,99 @@ var regions2 = [
 	["Phoenix", [
 		"Chandler",
 		"Phoenix"]]
-];
+];*/
+
+// Regions Object: State > Area > City > Property
+
+var regions3 = {
+	"states": [
+		{
+			"Arizona": [
+				{
+					"Phoenix Area": [
+						{
+							"Chandler": [
+								{
+									"property": "Pinnacle Queen Creek"
+								},
+								{
+									"property": "Pinnacle Terrace"
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			"California": [
+				{
+					"Inland Empire": [
+						{
+							"Chino": [
+								{
+									"property": "Enclave at Town Square"
+								}
+							]
+						},
+						{
+							"Chino Hills": [
+								{
+									"property": "The Heights"
+								},
+								{
+									"property": "The Summit"
+								}
+							]
+						},
+						{
+							"Colton": [
+								{
+									"property": "Windrush Village"
+								}
+							]
+						},
+						{
+							"Diamond Bar": [
+								{
+									"property": "Emerald Pointe"
+								}
+							]
+						},
+						{
+							"Moreno Valley": [
+								{
+									"property": "Galleria at Towngate"
+								}
+							]
+						},
+						{
+							"Riverside": [
+								{
+									"property": "Mission Grove Park"
+								}
+							]
+						}
+					]
+				},
+				{
+					"Los Angeles County": [
+						{
+							"Los Angeles": [
+								{
+									"property": "5600 Wilshire"
+								}
+							]
+						}
+					]
+				}
+			]
+		},
+		{
+			"state": "Denver"
+		},
+		{
+			"state": "Washington"
+		}
+	]
+};
